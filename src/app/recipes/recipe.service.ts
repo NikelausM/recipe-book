c90d55e1aa255ad94f5086ec8b30067ed185bdbc
Nicolas Mora <jnickm@gmail.com>
2021-10-26 08:59:53 -0400

    Add http handling

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -8,39 +8,45 @@ import { Recipe } from "./recipe.model";
 export class RecipeService {
   recipesChanged = new Subject<Recipe[]>();
 
-  recipes: Recipe[] = [
-    new Recipe('Sweet Potatoes', 
-      'Delicious sweet potatoes.', 'https://www.simplyrecipes.com/thmb/IbY_MK3pA1G_ZHzWltgZCxe_OUk=/960x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/__opt__aboutcom__coeus__resources__content_migration__simply_recipes__uploads__2012__07__grilled-sweet-potatoes-vertical-a-1600-0eafb4cd27b74617abb36379751eed51.jpg',
-      [
-        new Ingredient('Sweet Potato', 1),
-        new Ingredient('Parsley Fries', 1)
-      ]),
-    new Recipe('Burger', 
-      'Savoury burger.', 'https://natashaskitchen.com/wp-content/uploads/2019/04/Best-Burger-5.jpg',
-      [
-        new Ingredient('Ground Chuck Beef', 1),
-        new Ingredient('Salt', 1),
-        new Ingredient('Burger Bun', 1),
-        new Ingredient('Sliced Chedder Cheese', 2),
-        new Ingredient('Large Tomato', 0.5),
-        new Ingredient('Green Leaf Lettuce', 1),
-        new Ingredient('Medium Red Onion', 0.5),
-        new Ingredient('Dill Pick Slices', 2),
-      ]),
-    new Recipe('Sandwich', 
-      'Amazing sandwich.', 'https://assets.bonappetit.com/photos/59aec99643509c1d8b9371ee/6:9/w_3054,h_4581,c_limit/20170815%20MOB14283.jpg',
-      [
-        new Ingredient('Avocado', 1),
-        new Ingredient('Bread Slice', 2),
-        new Ingredient('Mozarella Cheese', 3),
-        new Ingredient('Lettuce Leaf', 3)
-      ])
-  ];
+  // recipes: Recipe[] = [
+  //   new Recipe('Sweet Potatoes', 
+  //     'Delicious sweet potatoes.', 'https://www.simplyrecipes.com/thmb/IbY_MK3pA1G_ZHzWltgZCxe_OUk=/960x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/__opt__aboutcom__coeus__resources__content_migration__simply_recipes__uploads__2012__07__grilled-sweet-potatoes-vertical-a-1600-0eafb4cd27b74617abb36379751eed51.jpg',
+  //     [
+  //       new Ingredient('Sweet Potato', 1),
+  //       new Ingredient('Parsley Fries', 1)
+  //     ]),
+  //   new Recipe('Burger', 
+  //     'Savoury burger.', 'https://natashaskitchen.com/wp-content/uploads/2019/04/Best-Burger-5.jpg',
+  //     [
+  //       new Ingredient('Ground Chuck Beef', 1),
+  //       new Ingredient('Salt', 1),
+  //       new Ingredient('Burger Bun', 1),
+  //       new Ingredient('Sliced Chedder Cheese', 2),
+  //       new Ingredient('Large Tomato', 0.5),
+  //       new Ingredient('Green Leaf Lettuce', 1),
+  //       new Ingredient('Medium Red Onion', 0.5),
+  //       new Ingredient('Dill Pick Slices', 2),
+  //     ]),
+  //   new Recipe('Sandwich', 
+  //     'Amazing sandwich.', 'https://assets.bonappetit.com/photos/59aec99643509c1d8b9371ee/6:9/w_3054,h_4581,c_limit/20170815%20MOB14283.jpg',
+  //     [
+  //       new Ingredient('Avocado', 1),
+  //       new Ingredient('Bread Slice', 2),
+  //       new Ingredient('Mozarella Cheese', 3),
+  //       new Ingredient('Lettuce Leaf', 3)
+  //     ])
+  // ];
+  private recipes: Recipe[] = [];
 
   constructor(private shoppingListService: ShoppingListService) {}
 
   getRecipes(): Recipe[] {
     return this.recipes.slice();
+  }
+
+  setRecipes(recipes: Recipe[]) {
+    this.recipes = recipes;
+    this.recipesChanged.next(this.recipes.slice());
   }
 
   getRecipe(index: number) {
---
+++ src/app/shared/data-storage.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from '@angular/common/http'
+import { RecipeService } from "../recipes/recipe.service";
+import { Recipe } from "../recipes/recipe.model";
+import { map, tap } from "rxjs/operators";
+import { Observable } from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataStorageService {
+
+  constructor(private http: HttpClient, private recipeService: RecipeService) { }
+
+  static get DATA_URL(): string {
+    return 'https://ng-course-recipe-book-2d809-default-rtdb.firebaseio.com/';
+  }
+
+  static get recipesURL(): string {
+    return [DataStorageService.DATA_URL, 'recipes.json'].join('/');
+  }
+
+  storeRecipes() {
+    const recipes = this.recipeService.getRecipes();
+    this.http.put(DataStorageService.recipesURL, recipes)
+      .subscribe(response => {
+        console.log(response);
+      });
+  }
+
+  fetchRecipes(): Observable<Recipe[]> {
+    console.log(DataStorageService.recipesURL);
+
+    return this.http.get<Recipe[]>(DataStorageService.recipesURL)
+      .pipe(
+        map(recipes => {
+          return recipes.map(recipe => {
+            return { ...recipe, ingredients: recipe.ingredients ? recipe.ingredients : [] }
+          });
+        }),
+        tap(recipes => {
+          this.recipeService.setRecipes(recipes);
+        })
+      )
+  }
+}
