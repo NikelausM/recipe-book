83b8759be1f89f6ac883d31d01c7cb94a737f2a2
Nicolas Mora <jnickm@gmail.com>
2021-12-08 21:59:19 -0500

    Update project name and add Angular Universal for server side rendering on first page load

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 import { LoggingService } from './logging.service';
 import { Store } from '@ngrx/store';
 import * as fromApp from './store/app.reducer';
@@ -12,12 +13,15 @@ import * as AuthActions from './auth/store/auth.actions';
 export class AppComponent implements OnInit {
   constructor(
     private store: Store<fromApp.AppState>,
-    private loggingService: LoggingService
+    private loggingService: LoggingService,
+    @Inject(PLATFORM_ID) private platformId
     ) {}
 
   ngOnInit(): void {
     console.log("[app.component.ngOnInit] starting");
-    this.store.dispatch(new AuthActions.AutoLogin());
+    if (isPlatformBrowser(this.platformId)) {
+      this.store.dispatch(new AuthActions.AutoLogin());
+    }
     this.loggingService.printLog("Hello from AppComponent ngOnInit");
   }
   
